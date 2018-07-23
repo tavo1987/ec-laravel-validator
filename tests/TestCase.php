bc16b1f3c05e980b8d049df969669dfb77ee2ac0
tavo <tavo198718@gmail.com>
2018-07-22 21:36:16 -0500

    Add namespace prefix to tests suite

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests;
+namespace Tavo\EcLaravelValidator\Tests;
 
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 use Tavo\EcLaravelValidator\EcValidatorServiceProvider;
